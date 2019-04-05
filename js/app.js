6dffb64e84ce3688b52fa7fb3554b0521a79b5cd
Jhrohman <josephhrohman@gmail.com>
2019-04-05 15:27:42 -0700

    Created: initial screen, added wireframe, added css, added js.

---
+++ js/app.js
@@ -0,0 +1,66 @@
+let x = 0;
+// Page reveal__________________________________________________________________________________________________
+$('#unlock').click(function(){
+        $('.hold').css('display','block');
+        $('.key').css('display','none');
+})
+// Interests reveal / hide__________________________________________________________________________________________________
+$('#interests').click(function(){
+    if(x === 0){
+        $('.colInterests').css('display','block');
+        x += 1;
+    } else {
+        $('.colInterests').css('display','none');
+        x -= 1;
+    }
+})
+// Projects reveal / hide__________________________________________________________________________________________________
+$('#projects').click(function(){
+    if(x === 0){
+        $('.colProjects').css('display','block');
+        x += 1;
+    } else {
+        $('.colProjects').css('display','none');
+        x -= 1;
+    }
+})
+// Story reveal / hide__________________________________________________________________________________________________
+$('#story').click(function(){
+    if(x === 0){
+        $('.colStory').css('display','block');
+        x += 1;
+    } else {
+        $('.colStory').css('display','none');
+        x -= 1;
+    }
+})
+// Contact reveal / hide__________________________________________________________________________________________________
+$('#contact').click(function(){
+    if(x === 0){
+        $('.colContact').css('display','block');
+        x += 1;
+    } else {
+        $('.colContact').css('display','none');
+        x -= 1;
+    }
+})
+
+// Carousel: Project__________________________________________________________________________________________________
+
+$('#1').click(function(){
+    $('#1').attr('class','layer1');
+    $('#2').attr('class','layer2');
+    $('#3').attr('class','layer2');
+})
+$('#2').click(function(){
+    $('#2').attr('class','layer1');
+    $('#1').attr('class','layer2');
+    $('#3').attr('class','layer2');
+})
+$('#3').click(function(){
+    $('#3').attr('class','layer1');
+    $('#2').attr('class','layer2');
+    $('#1').attr('class','layer2');
+})
+
+// 
