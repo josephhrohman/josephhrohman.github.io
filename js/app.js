9caaa87bc4d6abfd46150c12bb8c7e8179e62f0a
Jhrohman <josephhrohman@gmail.com>
2019-09-30 11:53:04 -0700

    Homepage work

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
 window.onload = function () {
 
-  document.getElementById('webPageSelector1').addEventListener('click', function(){
-    document.getElementById('webPageSelector1').classList.add('web-page-selector-focused');
-    document.getElementById('webPageSelector1').classList.remove('web-page-selector-unfocused');
-    document.getElementById('webPageSelector2').classList.add('web-page-selector-unfocused');
-    document.getElementById('webPageSelector2').classList.remove('web-page-selector-focused');
-    document.getElementById('webPageSelector3').classList.add('web-page-selector-unfocused');
-    document.getElementById('webPageSelector3').classList.remove('web-page-selector-focused');
-    document.getElementById('webPage1').classList.remove('web-page-unfocused');
-    document.getElementById('webPage1').classList.add('web-page-focused');
-    document.getElementById('webPage2').classList.add('web-page-unfocused');
-    document.getElementById('webPage3').classList.add('web-page-unfocused');
-  });
+  // document.getElementById('webPageSelector1').addEventListener('click', function(){
+  //   document.getElementById('webPageSelector1').classList.add('web-page-selector-focused');
+  //   document.getElementById('webPageSelector1').classList.remove('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector2').classList.add('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector2').classList.remove('web-page-selector-focused');
+  //   document.getElementById('webPageSelector3').classList.add('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector3').classList.remove('web-page-selector-focused');
+  //   document.getElementById('webPage1').classList.remove('web-page-unfocused');
+  //   document.getElementById('webPage1').classList.add('web-page-focused');
+  //   document.getElementById('webPage2').classList.add('web-page-unfocused');
+  //   document.getElementById('webPage3').classList.add('web-page-unfocused');
+  // });
 
-  document.getElementById('webPageSelector2').addEventListener('click', function(){
-    document.getElementById('webPageSelector2').classList.add('web-page-selector-focused');
-    document.getElementById('webPageSelector2').classList.remove('web-page-selector-unfocused');
-    document.getElementById('webPageSelector1').classList.add('web-page-selector-unfocused');
-    document.getElementById('webPageSelector1').classList.remove('web-page-selector-focused');
-    document.getElementById('webPageSelector3').classList.add('web-page-selector-unfocused');
-    document.getElementById('webPageSelector3').classList.remove('web-page-selector-focused');
-    document.getElementById('webPage2').classList.remove('web-page-unfocused');
-    document.getElementById('webPage2').classList.add('web-page-focused');
-    document.getElementById('webPage1').classList.add('web-page-unfocused');
-    document.getElementById('webPage3').classList.add('web-page-unfocused');
-  });
+  // document.getElementById('webPageSelector2').addEventListener('click', function(){
+  //   document.getElementById('webPageSelector2').classList.add('web-page-selector-focused');
+  //   document.getElementById('webPageSelector2').classList.remove('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector1').classList.add('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector1').classList.remove('web-page-selector-focused');
+  //   document.getElementById('webPageSelector3').classList.add('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector3').classList.remove('web-page-selector-focused');
+  //   document.getElementById('webPage2').classList.remove('web-page-unfocused');
+  //   document.getElementById('webPage2').classList.add('web-page-focused');
+  //   document.getElementById('webPage1').classList.add('web-page-unfocused');
+  //   document.getElementById('webPage3').classList.add('web-page-unfocused');
+  // });
 
-  document.getElementById('webPageSelector3').addEventListener('click', function(){
-    document.getElementById('webPageSelector3').classList.add('web-page-selector-focused');
-    document.getElementById('webPageSelector3').classList.remove('web-page-selector-unfocused');
-    document.getElementById('webPageSelector2').classList.add('web-page-selector-unfocused');
-    document.getElementById('webPageSelector2').classList.remove('web-page-selector-focused');
-    document.getElementById('webPageSelector1').classList.add('web-page-selector-unfocused');
-    document.getElementById('webPageSelector1').classList.remove('web-page-selector-focused');
-    document.getElementById('webPage3').classList.remove('web-page-unfocused');
-    document.getElementById('webPage3').classList.add('web-page-focused');
-    document.getElementById('webPage1').classList.add('web-page-unfocused');
-    document.getElementById('webPage2').classList.add('web-page-unfocused');
-  });
+  // document.getElementById('webPageSelector3').addEventListener('click', function(){
+  //   document.getElementById('webPageSelector3').classList.add('web-page-selector-focused');
+  //   document.getElementById('webPageSelector3').classList.remove('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector2').classList.add('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector2').classList.remove('web-page-selector-focused');
+  //   document.getElementById('webPageSelector1').classList.add('web-page-selector-unfocused');
+  //   document.getElementById('webPageSelector1').classList.remove('web-page-selector-focused');
+  //   document.getElementById('webPage3').classList.remove('web-page-unfocused');
+  //   document.getElementById('webPage3').classList.add('web-page-focused');
+  //   document.getElementById('webPage1').classList.add('web-page-unfocused');
+  //   document.getElementById('webPage2').classList.add('web-page-unfocused');
+  // });
 
 };
