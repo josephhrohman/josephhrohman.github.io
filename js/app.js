58b726febda80b180f92d759a3bd20a8b0254e34
Jhrohman <josephhrohman@gmail.com>
2019-10-05 16:49:21 -0700

    Adds full modal functionality to about page

--- js/app.js
+++ js/app.js
@@ -1,49 +1,24 @@
 window.onload = function () {
 
-  // document.getElementById('navAbout').addEventListener('click', function(){
-  //   document.getElementById('aboutSection').classList.add('about');
-  //   document.getElementById('aboutSection').classList.remove('section-none');
-  //   document.getElementById('webSection').classList.add('section-none');
-  //   document.getElementById('webSection').classList.remove('web');
-  //   document.getElementById('productSection').classList.add('section-none');
-  //   document.getElementById('productSection').classList.remove('product');
-  //   document.getElementById('graphicSection').classList.add('section-none');
-  //   document.getElementById('graphicSection').classList.remove('graphic');
-  // });
+  document.getElementById('updateButtonOn').addEventListener('click', function(){
+    document.getElementById('updateModal').classList.add('update-on');
+    document.getElementById('updateModal').classList.remove('update-on-load');
+    document.getElementById('updateButtonOn').style.display = 'none';
+    document.getElementById('updateButtonOff').style.display = 'block';
+  });
 
-  // document.getElementById('navWeb').addEventListener('click', function(){
-  //   document.getElementById('webSection').classList.add('web');
-  //   document.getElementById('webSection').classList.remove('section-none');
-  //   document.getElementById('aboutSection').classList.add('section-none');
-  //   document.getElementById('aboutSection').classList.remove('about');
-  //   document.getElementById('productSection').classList.add('section-none');
-  //   document.getElementById('productSection').classList.remove('product');
-  //   document.getElementById('graphicSection').classList.add('section-none');
-  //   document.getElementById('graphicSection').classList.remove('graphic');
-  // });
+  document.getElementById('updateButtonOff').addEventListener('click', function(){
+    document.getElementById('updateModal').classList.add('update-off');
+    document.getElementById('updateModal').classList.remove('update-on');
+    document.getElementById('updateButtonOn').style.display = 'block';
+    document.getElementById('updateButtonOff').style.display = 'none';
+  });
 
-  // document.getElementById('navProduct').addEventListener('click', function(){
-  //   document.getElementById('productSection').classList.add('product');
-  //   document.getElementById('productSection').classList.remove('section-none');
-  //   document.getElementById('aboutSection').classList.add('section-none');
-  //   document.getElementById('aboutSection').classList.remove('about');
-  //   document.getElementById('webSection').classList.add('section-none');
-  //   document.getElementById('webSection').classList.remove('web');
-  //   document.getElementById('graphicSection').classList.add('section-none');
-  //   document.getElementById('graphicSection').classList.remove('graphic');
-  // });
-
-  // document.getElementById('navGraphic').addEventListener('click', function(){
-  //   document.getElementById('graphicSection').classList.add('graphic');
-  //   document.getElementById('graphicSection').classList.remove('section-none');
-  //   document.getElementById('aboutSection').classList.add('section-none');
-  //   document.getElementById('aboutSection').classList.remove('about');
-  //   document.getElementById('webSection').classList.add('section-none');
-  //   document.getElementById('webSection').classList.remove('web');
-  //   document.getElementById('productSection').classList.add('section-none');
-  //   document.getElementById('productSection').classList.remove('product');
-  // });
-
-  // popOutUpdate
-
+  document.getElementById('updateButtonOn').addEventListener('click', function(){
+    document.getElementById('updateModal').classList.add('update-on');
+    document.getElementById('updateModal').classList.remove('update-off');
+    document.getElementById('updateButtonOn').style.display = 'none';
+    document.getElementById('updateButtonOff').style.display = 'block';
+  });
+  
 };
