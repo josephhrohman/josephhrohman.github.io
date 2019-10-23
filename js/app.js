4c454be195e7077e8ef077498252f3d717725b0c
Jhrohman <josephhrohman@gmail.com>
2019-10-23 08:18:13 -0700

    Adds new index page to be built out later.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 window.onload = function () {
 
-  document.getElementById('updateButtonOn').addEventListener('click', function(){
-    document.getElementById('updateModal').classList.add('update-on');
-    document.getElementById('updateModal').classList.remove('update-on-load');
-    document.getElementById('updateButtonOn').style.display = 'none';
-    document.getElementById('updateButtonOff').style.display = 'block';
-  });
+  // document.getElementById('updateButtonOn').addEventListener('click', function(){
+  //   document.getElementById('updateModal').classList.add('update-on');
+  //   document.getElementById('updateModal').classList.remove('update-on-load');
+  //   document.getElementById('updateButtonOn').style.display = 'none';
+  //   document.getElementById('updateButtonOff').style.display = 'block';
+  // });
 
-  document.getElementById('updateButtonOff').addEventListener('click', function(){
-    document.getElementById('updateModal').classList.add('update-off');
-    document.getElementById('updateModal').classList.remove('update-on');
-    document.getElementById('updateButtonOn').style.display = 'block';
-    document.getElementById('updateButtonOff').style.display = 'none';
-  });
+  // document.getElementById('updateButtonOff').addEventListener('click', function(){
+  //   document.getElementById('updateModal').classList.add('update-off');
+  //   document.getElementById('updateModal').classList.remove('update-on');
+  //   document.getElementById('updateButtonOn').style.display = 'block';
+  //   document.getElementById('updateButtonOff').style.display = 'none';
+  // });
 
-  document.getElementById('updateButtonOn').addEventListener('click', function(){
-    document.getElementById('updateModal').classList.add('update-on');
-    document.getElementById('updateModal').classList.remove('update-off');
-    document.getElementById('updateButtonOn').style.display = 'none';
-    document.getElementById('updateButtonOff').style.display = 'block';
-  });
+  // document.getElementById('updateButtonOn').addEventListener('click', function(){
+  //   document.getElementById('updateModal').classList.add('update-on');
+  //   document.getElementById('updateModal').classList.remove('update-off');
+  //   document.getElementById('updateButtonOn').style.display = 'none';
+  //   document.getElementById('updateButtonOff').style.display = 'block';
+  // });
   
 };
