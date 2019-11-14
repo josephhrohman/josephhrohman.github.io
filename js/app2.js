69880ec1f0c1f6cf251e71cc2f425605c3d35d67
Jhrohman <josephhrohman@gmail.com>
2019-11-14 12:44:07 -0800

    Adds minor CSS and completes Javascript

--- js/app2.js
+++ js/app2.js
@@ -6,27 +6,27 @@ window.onload = function () {
   let c = 0.25;
   let d = 0.125;
   let e = 0.0625;
-  let f = 0.3125;
+  let f = 0.03125;
   let g = 0.015625;
+  let h = 0.0078125;
   let output = null;
   let hold = null;
 
-  const valueList = [a, b, c, d, e, f, g];
+  const valueList = [a, b, c, d, e, f, g, h];
 
   const modRunner2 = (num) => {
     num = document.getElementById('inputValue').value;
     hold = num%a;
     output = (num-hold)/a;
-    console.log(`There are ${output} ${a}\'s in ${num}`);
+    document.getElementById("inputNumber").innerHTML = `${num}`;
+    document.getElementById(valueList[0]).innerHTML = output;
     for (let i=1; i < valueList.length; i++) {
       num = hold;
       hold = num%valueList[i];
       output = (num-hold)/valueList[i];
-      console.log(`There are ${output} ${valueList[i]}\'s in ${valueList[i]}`);
+      document.getElementById(valueList[i]).innerHTML = output;
     };
   }
-
-  // document.getElementById('run').onclick = modRunner2;
 
   let run = document.getElementById("run");
   if (run.addEventListener)
