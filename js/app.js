5d8d2a7dd390dbb081be7f042f4ef7d24d49d183
Jhrohman <josephhrohman@gmail.com>
2019-12-11 22:13:57 -0800

    adds new panel type to product design css

--- js/app.js
+++ js/app.js
@@ -1,26 +1,21 @@
 window.onload = function () {
 
-  document.getElementById('imgId9')
+  // imgClick
 
-  // document.getElementById('updateButtonOn').addEventListener('click', function(){
-  //   document.getElementById('updateModal').classList.add('update-on');
-  //   document.getElementById('updateModal').classList.remove('update-on-load');
-  //   document.getElementById('updateButtonOn').style.display = 'none';
-  //   document.getElementById('updateButtonOff').style.display = 'block';
-  // });
+  // const modRunner2 = (num) => {
+  //   num = document.getElementById('inputValue').value;
+  //   hold = num%a;
+  //   output = (num-hold)/a;
+  //   document.getElementById("inputNumber").innerHTML = `${num}`;
+  //   document.getElementById(valueList[0]).innerHTML = output;
+  //   // document.getElementById('indicator').style.
+  //   for (let i=1; i < valueList.length; i++) {
+  //     num = hold;
+  //     hold = num%valueList[i];
+  //     output = (num-hold)/valueList[i];
+  //     document.getElementById(valueList[i]).innerHTML = output;
+  //   };
+  // }
 
-  // document.getElementById('updateButtonOff').addEventListener('click', function(){
-  //   document.getElementById('updateModal').classList.add('update-off');
-  //   document.getElementById('updateModal').classList.remove('update-on');
-  //   document.getElementById('updateButtonOn').style.display = 'block';
-  //   document.getElementById('updateButtonOff').style.display = 'none';
-  // });
-
-  // document.getElementById('updateButtonOn').addEventListener('click', function(){
-  //   document.getElementById('updateModal').classList.add('update-on');
-  //   document.getElementById('updateModal').classList.remove('update-off');
-  //   document.getElementById('updateButtonOn').style.display = 'none';
-  //   document.getElementById('updateButtonOff').style.display = 'block';
-  // });
   
 };
